f92d7aeafd4b79110dc0d119846d9b42c91398ef
Navin Tiwari <145213281+NavinTiwari767@users.noreply.github.com>
2024-08-08 20:22:56 +0530

    Add files via upload

---
+++ ROCK PAPER SCISSOR/javascript.js
@@ -0,0 +1,15 @@
+document.getElementById('PAPER').addEventListener('click',function(e)
+{
+alert("paper is clicked")
+
+})
+document.getElementById('ROCK').addEventListener('click',function(e)
+{
+alert("rock is clicked")
+
+})
+document.getElementById('SCISSOR').addEventListener('click',function(e)
+{
+alert("scissor is clicked")
+
+})
